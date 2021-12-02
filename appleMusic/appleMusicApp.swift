1aac98428396092999282d7e2af8d3ea4caf1ee0
Denis Snezhko <mikle4421@mail.ru>
2021-12-02 18:16:31 +0300

    Create project.

---
+++ appleMusic/appleMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  appleMusicApp.swift
+//  appleMusic
+//
+//  Created by Denis Snezhko on 02.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct appleMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
