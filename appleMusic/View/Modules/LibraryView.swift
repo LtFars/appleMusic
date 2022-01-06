9179b2873dfd2d9783e954b3fdd11fad8cb62845
Denis Snezhko <mikle4421@mail.ru>
2022-01-06 16:24:51 +0300

    Add gitignore changes.

--- appleMusic/View/Modules/LibraryView.swift
+++ appleMusic/View/Modules/LibraryView.swift
@@ -10,35 +10,39 @@ import SwiftUI
 struct LibraryView: View {
     var body: some View {
         NavigationView {
-            TabView {
-//                ScrollView {
-                    VStack {
-                        Text("Ищите свою музыку?")
-                        Text("Здесь появится пленная Вамив iTunes Store музыка")
+            ZStack {
+                TabView {
+    //                ScrollView {
+                        VStack {
+                            Text("Ищите свою музыку?")
+                            Text("Здесь появится пленная Вамив iTunes Store музыка")
+                        }
+    //                }
+                    .tabItem {
+                        Image(systemName: "rectangle.stack.badge.play")
                     }
-//                }
-                .tabItem {
-                    Image(systemName: "rectangle.stack.badge.play")
+                    ScrollView {
+                        Text("Радио")
+                    }
+                    .tabItem {
+                        Image(systemName: "dot.radiowaves.left.and.right")
+                    }
+                    
+                    ScrollView {
+                        Text("Поиск")
+                    }
+                    .tabItem {
+                        Image(systemName: "magnifyingglass")
+                    }
                 }
-                ScrollView {
-                    Text("Радио")
-                }
-                .tabItem {
-                    Image(systemName: "dot.radiowaves.left.and.right")
-                }
-                
-                ScrollView {
-                    Text("Поиск")
-                }
-                .tabItem {
-                    Image(systemName: "magnifyingglass")
-                }
+                .accentColor(.red)
+                .navigationTitle("Media")
+                .navigationBarItems(
+                    trailing: Button("Править") {}
+                                .foregroundColor(.red))
+                PlayerView()
+//                    .padding()
             }
-            .accentColor(.red)
-            .navigationTitle("Media")
-            .navigationBarItems(
-                trailing: Button("Править") {}
-                            .foregroundColor(.red))
         }
     }
 }
